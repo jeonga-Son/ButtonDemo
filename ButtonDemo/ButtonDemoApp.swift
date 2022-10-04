60e10adb939b440e2530f292310ffec3dbad7181
jeonga-Son <jjeong0106@gmail.com>
2022-10-04 08:42:00 -0400

    Initial Commit

---
+++ ButtonDemo/ButtonDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonDemoApp.swift
+//  ButtonDemo
+//
+//  Created by jeonga Son on 2022/10/04.
+//
+
+import SwiftUI
+
+@main
+struct ButtonDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
